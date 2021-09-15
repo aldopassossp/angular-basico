295486d95c8d2fd000a2dc5095649863437e8157
Aldo de Oliveira Passos <aldopassos@gmail.com>
2021-09-15 20:08:17 -0300

    Project Angular Basic

---
+++ angular-basico/course-manager/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { CourseModule } from './course/course.module';
+import { CoreModule } from './core/core-module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    CourseModule,
+    CoreModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      }
+    ]),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
